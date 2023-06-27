3898faa067434b44c2d0ad280850a241aaa417d5
jonathan966 <1720110004@utectulancingo.edu.mx>
2023-06-27 06:43:23 +0000

    commit

--- src/app.py
+++ src/app.py
@@ -6,7 +6,12 @@ app=Flask(__name__)
   
 @app.route('/')
 def index():
-    return render_template('auth/login.html')
+    return render_template('auth/iniciar.html')
+
+@app.route('/registro')
+def register():        
+    return render_template('auth/registro.html')
+
 
 if __name__=='__main__':
     app.config.from_object(config['development'])
