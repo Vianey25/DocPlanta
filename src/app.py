cb7dc6e60ca0f84998078c323a0d179a461ce630
jonathan966 <1720110004@utectulancingo.edu.mx>
2023-07-13 23:24:44 -0600

    login arreglado

--- src/app.py
+++ src/app.py
@@ -1,45 +1,53 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify,flash
+from functools import wraps
+from flask import Flask, render_template, request, redirect, url_for, jsonify, flash, session, Response
 from flask_mysqldb import MySQL
 from config import config
-
-from models.modeluser import ModelUser
-
-from models.entities.user import User
 
 app = Flask(__name__)
 
 db = MySQL(app)
 
+# Decorator function to check login status
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'logeado' not in session or not session['logeado']:
+            flash('Inicia sesión para acceder a esta página', 'error')
+            return redirect(url_for('index'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return redirect(url_for('login'))
+    return render_template('auth/iniciar.html')
 
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/acceso-login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        email = User(0,request.form['email'],request.form['password'])
-        logged_user = ModelUser.login(db,email)
-        if logged_user != None :
-            if logged_user.password:
-                return render_template('auth/home.html')
-            else:
-                flash("contraseña no valida --")
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+        cur = db.connection.cursor()
+        cur.execute('SELECT * FROM user WHERE email = %s AND password = %s', (email, password))
+        account = cur.fetchone()
+
+        if account:
+            session['logeado'] = True
+            session['id'] = account[0]
+            return render_template('auth/home.html')
         else:
-            flash("usuario no encontrado")
-        return render_template('auth/iniciar.html')
-    else:
-        return render_template('auth/iniciar.html')
+            flash('Email o contraseña inválidos', 'error')
+            return render_template('auth/iniciar.html')
 
 @app.route('/home')
-def register1():
+@login_required  # Apply login_required decorator to the route
+def home():
     return render_template('auth/home.html')
 
+@app.route('/registro')
+def register1():
+    return render_template('auth/registro.html')
 
-
-
-@app.route('/registro', methods=['GET', 'POST'])
+@app.route('/accion-registro', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         email = request.form['email']
@@ -47,33 +55,37 @@ def register():
         cur = db.connection.cursor()
         cur.execute('INSERT INTO user (email, password) VALUES (%s, %s)', (email, password))
         db.connection.commit()
-        flash("añadido")
-        return redirect(url_for('register'))
-    return render_template('auth/registro.html')
+        flash("El usuario ha sido registrado")
+        return render_template('auth/registro.html')
 
-
+# Apply login_required decorator to the following routes
 @app.route('/plantas')
+@login_required
 def plantas():
     return render_template('auth/plantas.html')
 
-
 @app.route('/enfermedades')
+@login_required
 def enfermedades():
     return render_template('auth/enfermedades.html')
 
 @app.route('/invernadero')
+@login_required
 def invernadero():
     return render_template('auth/invernadero.html')
 
 @app.route('/temperatura-humedad')
+@login_required
 def temperatura_humedad():
     return render_template('auth/temperatura_humedad.html')
 
 @app.route('/sensores')
+@login_required
 def sensores():
     return render_template('auth/sensores.html')
 
 @app.route('/cursos')
+@login_required
 def cursos():
     return render_template('auth/cursos.html')
 
